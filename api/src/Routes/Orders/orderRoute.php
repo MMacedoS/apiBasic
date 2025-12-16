209cc5574f43806691e01b9f3618d036210d797a
Mauricio Macedo da Silva <macedo.mauriciogen@gmail.com>
2025-12-16 19:06:50 -0300

    feat: ajuste de semantica

--- api/src/Routes/Orders/orderRoute.php
+++ api/src/Routes/Orders/orderRoute.php
@@ -11,18 +11,26 @@ Route::group(['middleware' => ['auth', 'role' => 'admin']], function () {
     Route::delete('/orders/{uuid}', [OrdemController::class, 'destroy']);
 });
 
-Route::group(['middleware' => ['auth', 'permission' => [
-    'view_orders',
-    'view-own-order',
-    'assign-order',
-    'remove-order',
-    'view-my-order',
-    'view-my-service',
-    'view-my-customer'
-]]], function () {
-    Route::get('/orders-list', [OrdemController::class, 'indexWithoutPagination']);
-    Route::post('/orders/{uuid}/assign-service', [OrdemController::class, 'assignServiceToOrder']);
-    Route::delete('/orders/{uuid}/remove-service/{uuidService}', [OrdemController::class, 'removeServiceFromOrder']);
-    Route::post('/orders/{uuid}/assign-product', [OrdemController::class, 'assignProductToOrder']);
-    Route::delete('/orders/{uuid}/remove-product/{uuidProduct}', [OrdemController::class, 'removeProductFromOrder']);
-});
+Route::group(
+    [
+        'middleware' => [
+            'auth',
+            'permission' => [
+                'view_orders',
+                'view-own-order',
+                'assign-order',
+                'remove-order',
+                'view-my-order',
+                'view-my-service',
+                'view-my-customer'
+            ]
+        ]
+    ],
+    function () {
+        Route::get('/orders-list', [OrdemController::class, 'indexWithoutPagination']);
+        Route::post('/orders/{uuid}/assign-service', [OrdemController::class, 'assignServiceToOrder']);
+        Route::delete('/orders/{uuid}/remove-service/{uuidService}', [OrdemController::class, 'removeServiceFromOrder']);
+        Route::post('/orders/{uuid}/assign-product', [OrdemController::class, 'assignProductToOrder']);
+        Route::delete('/orders/{uuid}/remove-product/{uuidProduct}', [OrdemController::class, 'removeProductFromOrder']);
+    }
+);
